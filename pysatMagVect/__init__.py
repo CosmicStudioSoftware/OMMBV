976c607450de3100093bdf30c3e539efa949fe31
Russell Stoneback <rstoneba@utdallas.edu>
2018-06-21 22:57:54 -0500

    Update __init__.py

--- pysatMagVect/__init__.py
+++ pysatMagVect/__init__.py
@@ -5,9 +5,8 @@ with open(os.path.join(here, 'version.txt')) as version_file:
     __version__ = version_file.read().strip()
 del here
 
-
+from . import igrf
 from . import _core
 from ._core import *
-from . import igrf
 
 __all__ = []
