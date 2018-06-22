8472547d0b989661cd23fd1a230fc79e414b7043
rstoneback <rstoneba@utdallas.edu>
2018-06-21 22:11:09 -0500

    initial setup

---
+++ setup.py
@@ -0,0 +1,56 @@
+# Copyrighted by The University of Texas at Dallas subject to the United States 
+# government unlimited rights (DFAR 252.227-7013-5).
+
+import setuptools
+import numpy.distutils.core
+import os
+import sys
+import numpy as np
+
+from numpy.distutils.misc_util import Configuration
+from distutils.command.sdist import sdist
+
+
+#import setuptools
+
+# setup fortran extensions 
+
+
+config = Configuration('pysatMagVect')
+
+# create extension for calling IGRF
+config.add_extension(name='igrf',
+    sources = [os.path.join('pysatMagVect', 'igrf12.f')]
+    )
+
+here = os.path.abspath(os.path.dirname(__file__))
+#with open(path.join(here, 'description.txt'), encoding='utf-8') as f:
+#    long_description = f.read()
+with open(os.path.join(here, 'pysatMagVect', 'version.txt')) as version_file:
+    version = version_file.read().strip()
+
+# call setup
+#--------------------------------------------------------------------------
+numpy.distutils.core.setup( 
+
+    name = 'pysatMagVect',
+    version = version,        
+    packages = ['pysatMagVect','pysatMagVect.tests'],
+    description= ''.join(('Calculates geomagnetic unit vectors (field aligned, zonal, and meridional) '
+                 'and includes supporting routines for characterizing the motion of ionospheric plasma.')),
+    cmdclass={'sdist': sdist},
+   
+    # Author details
+    author='Russell Stoneback',
+    author_email='rstoneba@utdallas.edu',
+    package_data={'pysatMagVect': ['version.txt'],},
+    include_package_data=True,
+    
+    ext_modules = config.todict()['ext_modules'],
+
+    install_requires = ['numpy', 'scipy'],
+
+)  
+
+
+
