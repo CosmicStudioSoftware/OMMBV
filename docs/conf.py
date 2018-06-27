6dfd6e07376cd3732e0a5d34e832d98a022bc21b
rstoneback <rstoneba@utdallas.edu>
2018-06-27 17:01:13 -0500

    Mocked scipy for RTD builds

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 
-MOCK_MODULES = ['pysatMagVect.igrf']
+MOCK_MODULES = ['pysatMagVect.igrf', 'scipy']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 #print sys.path
