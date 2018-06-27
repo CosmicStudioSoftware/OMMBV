025f4af0621ae8bc2d6735fd29ecb19f39539326
rstoneback <rstoneba@utdallas.edu>
2018-06-27 18:34:39 -0500

    Changed setup.py since numpy sdist is broken.

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ import sys
 import numpy as np
 
 from numpy.distutils.misc_util import Configuration
+from numpy.distutils.command.sdist import sdist as sdist
 
 config = Configuration('pysatMagVect')
 
@@ -16,33 +17,33 @@ config.add_extension(name='igrf',
     sources = [os.path.join('pysatMagVect', 'igrf12.f')]
     )
 
-here = os.path.abspath(os.path.dirname(__file__))
-#with open(path.join(here, 'description.txt'), encoding='utf-8') as f:
-#    long_description = f.read()
-version_filename = os.path.join('pysatMagVect', 'version.txt')
-with open(os.path.join(here, version_filename)) as version_file:
-    version = version_file.read().strip()
+# here = os.path.abspath(os.path.dirname(__file__))
+# #with open(path.join(here, 'description.txt'), encoding='utf-8') as f:
+# #    long_description = f.read()
+# version_filename = os.path.join('pysatMagVect', 'version.txt')
+# with open(os.path.join(here, version_filename)) as version_file:
+#     version = version_file.read().strip()
 
 # call setup
 #--------------------------------------------------------------------------
 numpy.distutils.core.setup( 
 
     name = 'pysatMagVect',
-    version = version,        
+    version = '0.1.3',        
     packages = ['pysatMagVect','pysatMagVect.tests'],
     description= ''.join(('Calculates geomagnetic unit vectors (field aligned, zonal, and meridional) '
                  'and includes supporting routines for characterizing the motion of ionospheric plasma.')),
-    # cmdclass={'sdist': sdist},
+    # cmdclass={'sdist': numpy.distutils.command.sdist},
    
     # Author details
     author='Russell Stoneback',
     author_email='rstoneba@utdallas.edu',
-    package_data={'pysatMagVect': ['pysatMagVect/version.txt'],},
-    include_package_data=True,
+    # package_data={'pysatMagVect': ['pysatMagVect/version.txt']},
+    # include_package_data=True,
     
     ext_modules = config.todict()['ext_modules'],
 
-    install_requires = ['numpy', 'scipy'],
+    install_requires = ['numpy', 'scipy',],
 
 )  
 
