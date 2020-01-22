c748846973e2f2f5d7e44d433fc81b36d12f2782
rstoneback <rstoneba@utdallas.edu>
2020-01-22 13:22:11 -0600

    TES: Expanded satellite testing

--- pysatMagVect/tests/test_satellite.py
+++ pysatMagVect/tests/test_satellite.py
@@ -21,3 +21,32 @@ class TestSatellite():
                  'unit_mer_ecef_x', 'unit_mer_ecef_y', 'unit_mer_ecef_z']
         for item in items:
             assert item in self.inst.data
+
+
+    def test_application_add_mag_drifts(self):
+        self.inst.load(2019, 365)
+        # create false orientation signal
+        self.inst['sc_xhat_x'] = 1.
+        self.inst['sc_xhat_y'] = 0.
+        self.inst['sc_xhat_z'] = 0.
+        self.inst['sc_yhat_x'] = 0.
+        self.inst['sc_yhat_y'] = 1.
+        self.inst['sc_yhat_z'] = 0.
+        self.inst['sc_zhat_x'] = 0.
+        self.inst['sc_zhat_y'] = 0.
+        self.inst['sc_zhat_z'] = 1.
+
+        pymv.satellite.add_mag_drift_unit_vectors(self.inst)
+        items = ['unit_zon_x', 'unit_zon_y', 'unit_zon_z',
+                 'unit_fa_x', 'unit_fa_y', 'unit_fa_z',
+                 'unit_mer_x', 'unit_mer_y', 'unit_mer_z']
+        for item in items:
+            assert item in self.inst.data
+
+        self.inst['iv_x'] = 150.
+        self.inst['iv_y'] = 50.
+        self.inst['iv_z'] = -50.
+        pymv.satellite.add_mag_drifts(self.inst)
+        items = ['iv_zon', 'iv_fa', 'iv_mer']
+        for item in items:
+            assert item in self.inst.data
