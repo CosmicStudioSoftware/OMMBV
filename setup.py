9e7e5e11e0d81790d79aeddd32f63f0d34e7da0d
Russell Stoneback <rstoneba@utdallas.edu>
2020-01-27 10:20:33 -0600

    STY: Updated formatting

--- setup.py
+++ setup.py
@@ -1,11 +1,7 @@
-# Copyrighted by The University of Texas at Dallas subject to the United States
-# government unlimited rights (DFAR 252.227-7013-5).
-
-import setuptools
 import numpy.distutils.core
 import os
 
-from numpy.distutils.core import setup, Extension
+from numpy.distutils.core import Extension
 
 
 # create extension for calling IGRF
@@ -15,8 +11,8 @@ extensions = [Extension(name='pysatMagVect.igrf',
                         sources = [os.path.join('pysatMagVect', '_coords.f')])]
 
 here = os.path.abspath(os.path.dirname(__file__))
-#with open(path.join(here, 'description.txt'), encoding='utf-8') as f:
-#    long_description = f.read()
+# with open(path.join(here, 'description.txt'), encoding='utf-8') as f:
+#     long_description = f.read()
 version_filename = os.path.join('pysatMagVect', 'version.txt')
 with open(os.path.join(here, version_filename)) as version_file:
     version = version_file.read().strip()
@@ -24,12 +20,11 @@ with open(os.path.join(here, version_filename)) as version_file:
 # call setup
 #--------------------------------------------------------------------------
 numpy.distutils.core.setup(
-# setuptools.setup(
-    name = 'pysatMagVect',
-    version = version,
-    packages = ['pysatMagVect','pysatMagVect.tests'],
-    description= ''.join(('Calculates geomagnetic unit vectors (field aligned, zonal, and meridional) '
-                 'and includes supporting routines for characterizing the motion of ionospheric plasma.')),
+    name='pysatMagVect',
+    version=version,
+    packages=['pysatMagVect','pysatMagVect.tests'],
+    description=''.join(('Calculates geomagnetic unit vectors (field aligned, zonal, and meridional) '
+                          'and includes supporting routines for characterizing the motion of ionospheric plasma.')),
     url='http://github.com/pysat/pysatMagVect',
 
     # Author details
@@ -39,7 +34,6 @@ numpy.distutils.core.setup(
     include_package_data=True,
 
     # required modules
-    install_requires = ['numpy', 'scipy'],
-    ext_modules = extensions,
-
+    install_requires=['numpy', 'scipy'],
+    ext_modules=extensions,
 )
