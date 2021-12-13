8548f58dea28437e7909982b61705ca13205225f
Russell Stoneback <rstoneba@utdallas.edu>
2021-12-13 14:36:30 -0600

    TST: Added `test_trans.py` and filled with tests.

---
+++ OMMBV/tests/test_trans.py
@@ -0,0 +1,292 @@
+from nose.tools import assert_almost_equals as asseq
+
+import numpy as np
+
+import OMMBV
+import OMMBV.trans
+from OMMBV import igrf
+import pysat
+
+from OMMBV.tests.test_core import gen_data_fixed_alt
+
+
+# ############## TRANSFORMATIONS ############## #
+
+class TestTransformations():
+
+    def __init__(self):
+        # placeholder for data management features
+        self.inst = pysat.Instrument('pysat', 'testing')
+        self.inst.yr = 2010.
+        self.inst.doy = 1.
+
+        # Locations to perform tests at
+        self.lats, self.longs, self.alts = gen_data_fixed_alt(550.)
+
+        return
+
+    def test_geocentric_to_ecef_to_geocentric(self):
+        """Test Geocentric and ECEF transformations, round trip equality"""
+
+        ecf_x, ecf_y, ecf_z = OMMBV.trans.geocentric_to_ecef(self.lats,
+                                                             self.longs,
+                                                             self.alts)
+
+        lat, elong, alt = OMMBV.trans.ecef_to_geocentric(ecf_x, ecf_y, ecf_z)
+
+        idx, = np.where(elong < 0)
+        elong[idx] += 360.
+
+        d_lat = lat - self.lats
+        d_long = elong - self.longs
+        d_alt = alt - self.alts
+
+        assert np.all(np.abs(d_lat) < 1.E-5)
+        assert np.all(np.abs(d_long) < 1.E-5)
+        assert np.all(np.abs(d_alt) < 1.E-5)
+
+        return
+
+    def test_geocentric_to_ecef_floats_and_list(self):
+        """Geocentric and ECEF transformations different input types"""
+
+        ecf_x, ecf_y, ecf_z = OMMBV.trans.geocentric_to_ecef(self.lats[:1],
+                                                             self.longs[:1],
+                                                             self.alts[:1])
+
+        ecf_x2, ecf_y2, ecf_z2 = OMMBV.trans.geocentric_to_ecef(self.lats[0],
+                                                                self.longs[0],
+                                                                self.alts[0])
+        d_x = ecf_x2 - ecf_x
+        d_y = ecf_x2 - ecf_x
+        d_z = ecf_x2 - ecf_x
+
+        assert np.all(np.abs(d_x) < 1.E-5)
+        assert np.all(np.abs(d_y) < 1.E-5)
+        assert np.all(np.abs(d_z) < 1.E-5)
+
+        return
+
+    def test_ecef_to_geocentric_floats_and_list(self):
+        """Geocentric and ECEF transformations with different input types"""
+
+        # Generate ECEF starting locations
+        secf_x, secf_y, secf_z = OMMBV.trans.geocentric_to_ecef(self.lats[:1],
+                                                                self.longs[:1],
+                                                                self.alts[:1])
+        # Convert to geo with list input
+        ecf_x, ecf_y, ecf_z = OMMBV.trans.ecef_to_geocentric(secf_x[:1],
+                                                             secf_y[:1],
+                                                             secf_z[:1])
+
+        # Convert to geo with float input
+        ecf_x2, ecf_y2, ecf_z2 = OMMBV.trans.geocentric_to_ecef(secf_x[0],
+                                                                secf_y[0],
+                                                                secf_z[0])
+        d_x = ecf_x2 - ecf_x
+        d_y = ecf_x2 - ecf_x
+        d_z = ecf_x2 - ecf_x
+
+        assert np.all(np.abs(d_x) < 1.E-5)
+        assert np.all(np.abs(d_y) < 1.E-5)
+        assert np.all(np.abs(d_z) < 1.E-5)
+
+        return
+
+    def test_geodetic_to_ecef_floats_and_list(self):
+        """Geodetic and ECEF transformations different input types"""
+
+        ecf_x, ecf_y, ecf_z = OMMBV.trans.geodetic_to_ecef(self.lats[:1],
+                                                           self.longs[:1],
+                                                           self.alts[:1])
+
+        ecf_x2, ecf_y2, ecf_z2 = OMMBV.trans.geodetic_to_ecef(self.lats[0],
+                                                              self.longs[0],
+                                                              self.alts[0])
+
+        d_x = ecf_x2 - ecf_x
+        d_y = ecf_x2 - ecf_x
+        d_z = ecf_x2 - ecf_x
+
+        assert np.all(np.abs(d_x) < 1.E-5)
+        assert np.all(np.abs(d_y) < 1.E-5)
+        assert np.all(np.abs(d_z) < 1.E-5)
+
+        return
+
+    def test_ecef_to_geodetic_floats_and_list(self):
+        """Geodetic and ECEF transformations different input types"""
+
+        # Generate ECEF starting locations
+        secf_x, secf_y, secf_z = OMMBV.trans.geodetic_to_ecef(self.lats[:1],
+                                                              self.longs[:1],
+                                                              self.alts[:1])
+        # Convert to geo with list input
+        ecf_x, ecf_y, ecf_z = OMMBV.trans.ecef_to_geodetic(secf_x[:1],
+                                                           secf_y[:1],
+                                                           secf_z[:1])
+
+        # Convert to geo with float input
+        ecf_x2, ecf_y2, ecf_z2 = OMMBV.trans.geodetic_to_ecef(secf_x[0],
+                                                              secf_y[0],
+                                                              secf_z[0])
+        d_x = ecf_x2 - ecf_x
+        d_y = ecf_x2 - ecf_x
+        d_z = ecf_x2 - ecf_x
+
+        assert np.all(np.abs(d_x) < 1.E-5)
+        assert np.all(np.abs(d_y) < 1.E-5)
+        assert np.all(np.abs(d_z) < 1.E-5)
+
+        return
+
+    def test_geodetic_to_ecef_to_geodetic(self):
+        """Test geodetic to ECEF and back, both fortran and python"""
+
+        ecf_x, ecf_y, ecf_z = OMMBV.trans.geodetic_to_ecef(self.lats,
+                                                           self.longs,
+                                                           self.alts)
+
+        lat, elong, alt = OMMBV.trans.ecef_to_geodetic(ecf_x, ecf_y, ecf_z)
+
+        lat2, elong2, alt2 = OMMBV.trans.python_ecef_to_geodetic(ecf_x, ecf_y,
+                                                                 ecf_z)
+
+        idx, = np.where(elong < 0)
+        elong[idx] += 360.
+
+        idx, = np.where(elong2 < 0)
+        elong2[idx] += 360.
+
+        d_lat = lat - self.lats
+        d_long = elong - self.longs
+        d_alt = alt - self.alts
+
+        assert np.all(np.abs(d_lat) < 1.E-5)
+        assert np.all(np.abs(d_long) < 1.E-5)
+        assert np.all(np.abs(d_alt) < 1.E-5)
+
+        d_lat = lat2 - self.lats
+        d_long = elong2 - self.longs
+        d_alt = alt2 - self.alts
+
+        assert np.all(np.abs(d_lat) < 1.E-5)
+        assert np.all(np.abs(d_long) < 1.E-5)
+        assert np.all(np.abs(d_alt) < 1.E-5)
+
+        return
+
+    def test_geodetic_to_ecef_to_geodetic_via_different_methods(self):
+        """Multiple techniques for geodetic to ECEF to geodetic"""
+
+        ecf_x, ecf_y, ecf_z = OMMBV.trans.geodetic_to_ecef(self.lats,
+                                                           self.longs,
+                                                           self.alts)
+        methods = ['closed', 'iterative']
+        for method in methods:
+            lat, elong, alt = OMMBV.trans.python_ecef_to_geodetic(ecf_x, ecf_y,
+                                                                  ecf_z,
+                                                                  method=method)
+
+            idx, = np.where(elong < 0)
+            elong[idx] += 360.
+
+            d_lat = lat - self.lats
+            d_long = elong - self.longs
+            d_alt = alt - self.alts
+
+            assert np.all(np.abs(d_lat) < 1.E-5)
+            assert np.all(np.abs(d_long) < 1.E-5)
+            assert np.all(np.abs(d_alt) < 1.E-5)
+
+        return
+
+    def test_geodetic_to_ecef_to_geocentric_to_ecef_to_geodetic(self):
+        """Test geodetic to ecef and geocentric transformations"""
+
+        ecf_x, ecf_y, ecf_z = OMMBV.trans.geodetic_to_ecef(self.lats,
+                                                           self.longs,
+                                                           self.alts)
+
+        geo_lat, geo_long, geo_alt = OMMBV.trans.ecef_to_geocentric(ecf_x,
+                                                                    ecf_y,
+                                                                    ecf_z)
+
+        ecfs_x, ecfs_y, ecfs_z = OMMBV.trans.geocentric_to_ecef(geo_lat,
+                                                                geo_long,
+                                                                geo_alt)
+
+        lat, elong, alt = OMMBV.ecef_to_geodetic(ecfs_x, ecfs_y, ecfs_z)
+
+        idx, = np.where(elong < 0)
+        elong[idx] += 360.
+
+        d_lat = lat - self.lats
+        d_long = elong - self.longs
+        d_alt = alt - self.alts
+
+        assert np.all(np.abs(d_lat) < 1.E-5)
+        assert np.all(np.abs(d_long) < 1.E-5)
+        assert np.all(np.abs(d_alt) < 1.E-5)
+        assert np.all(np.abs(ecf_x - ecfs_x) < 1.E-5)
+        assert np.all(np.abs(ecf_y - ecfs_y) < 1.E-5)
+        assert np.all(np.abs(ecf_z - ecfs_z) < 1.E-5)
+
+        return
+
+    def test_igrf_ecef_to_geodetic_back_to_ecef(self):
+        """Test IGRF_ECEF - Geodetic - and Back"""
+
+        ecf_x, ecf_y, ecf_z = OMMBV.trans.geodetic_to_ecef(self.lats,
+                                                           self.longs,
+                                                           self.alts)
+        for (ecef_x, ecef_y, ecef_z,
+             geo_lat, geo_lon, geo_alt) in zip(ecf_x, ecf_y, ecf_z,
+                                               self.lats, self.longs,
+                                               self.alts):
+
+            pos = np.array([ecef_x, ecef_y, ecef_z])
+
+            lat, elong, alt = igrf.ecef_to_geodetic(pos)
+            lat = np.rad2deg(lat)
+            elong = np.rad2deg(elong)
+            if (elong < 0):
+                elong = elong + 360.
+
+            d_lat = lat - geo_lat
+            d_long = elong - geo_lon
+            d_alt = alt - geo_alt
+
+            assert np.all(np.abs(d_lat) < 1.E-5)
+            assert np.all(np.abs(d_long) < 1.E-5)
+            assert np.all(np.abs(d_alt) < 1.E-5)
+
+        return
+
+    def test_igrf_ecef_to_geographic_with_colatitude(self):
+        """Test IGRF_ECEF - Geographic"""
+
+        ecf_x, ecf_y, ecf_z = OMMBV.trans.geodetic_to_ecef(self.lats,
+                                                           self.longs,
+                                                           self.alts)
+
+        for (ecef_x, ecef_y, ecef_z,
+             geo_lat, geo_lon, geo_alt) in zip(ecf_x, ecf_y, ecf_z,
+                                               self.lats, self.longs,
+                                               self.alts):
+
+            pos = np.array([ecef_x, ecef_y, ecef_z])
+
+            colat, lon, r = igrf.ecef_to_colat_long_r(pos)
+
+            # Results are returned in radians
+            lat = 90. - np.rad2deg(colat)
+            lon = np.rad2deg(lon)
+
+            lat2, lon2, h2 = OMMBV.trans.ecef_to_geocentric(*pos, ref_height=0)
+
+            asseq(r, h2, 9)
+            asseq(lat, lat2, 9)
+            asseq(lon, lon2, 9)
+
+        return
