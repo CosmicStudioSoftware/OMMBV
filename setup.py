eeebd8ec9e294705fe0e56e8d21e34f5c23cd3e5
Russell Stoneback <rstoneba@utdallas.edu>
2021-12-22 19:22:27 -0600

    STY: Tweaks to setup

--- setup.py
+++ setup.py
@@ -34,38 +34,5 @@ else:
                   Extension(name='OMMBV.fortran_coords',
                             sources=[os.path.join('OMMBV', '_coords.f')])]
 
-setup(name='OMMBV',
-      version=version,
-      license='BSD-3-Clause',
-      packages=['OMMBV'],
-      description=' '.join(('Orthogonal geomagnetic vector basis and',
-                            'field-line mapping for multipole magnetic',
-                            'fields.')),
-      url='https://github.com/rstoneback/OMMBV',
-
-      # Author details
-      author='Russell Stoneback',
-      author_email='russell@stoneris.com',
-
-      # data_files=[('OMMBV', )],
-      # include_package_data=True,
-      classifiers=[
-          # complete classifier list:
-          # http://pypi.python.org/pypi?%3Aaction=list_classifiers
-          'Development Status :: 5 - Production/Stable',
-          'Intended Audience :: Science/Research',
-          'License :: OSI Approved :: BSD-3-Clause License',
-          'Operating System :: Unix',
-          'Operating System :: POSIX',
-          'Operating System :: Microsoft :: Windows',
-          'Operating System :: MacOS :: MacOS X',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: Implementation :: CPython',
-          'Topic :: Scientific/Engineering :: Physics',
-          'Topic :: Utilities',
-      ],
-
-      # Required modules
-      install_requires=['numpy', 'scipy'],
-      ext_modules=extensions)
+setup_kwargs = {'ext_modules': extensions}
+setup(**setup_kwargs)
