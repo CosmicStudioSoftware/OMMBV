c22f5c54150d9754c290983b1bdbbb3532cc5cc7
Russell Stoneback <rstoneba@utdallas.edu>
2025-01-15 14:25:29 -0600

    BUG: Restore fortran imports

--- OMMBV/__init__.py
+++ OMMBV/__init__.py
@@ -6,22 +6,22 @@ __version__ = '1.0.2'
 # try:
 
 
-# from OMMBV import igrf
-# from OMMBV import fortran_coords
-# from OMMBV import sources
+from OMMBV import igrf
+from OMMBV import fortran_coords
+from OMMBV import sources
 # except ImportError:
 #     # Warning about lack of import handled in trans
 #     igrf, sources, fortran_coords = None, None, None
 
-# from OMMBV import satellite
-# from OMMBV import trace
-# from OMMBV import trans
-# from OMMBV import utils
-# from OMMBV import vector
-#
-# from OMMBV import _core
-# from OMMBV._core import *
-#
-# from OMMBV import heritage
-#
-# __all__ = ['heritage', 'satellite', 'trace', 'trans', 'utils', 'vector']
+from OMMBV import satellite
+from OMMBV import trace
+from OMMBV import trans
+from OMMBV import utils
+from OMMBV import vector
+
+from OMMBV import _core
+from OMMBV._core import *
+
+from OMMBV import heritage
+
+__all__ = ['heritage', 'satellite', 'trace', 'trans', 'utils', 'vector']
