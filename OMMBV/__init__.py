859738f510c14eaee396ca7388989cb0a6cae10a
Russell Stoneback <rstoneba@utdallas.edu>
2025-01-14 20:06:05 -0600

    BUG: Debugging import

--- OMMBV/__init__.py
+++ OMMBV/__init__.py
@@ -3,13 +3,13 @@
 
 __version__ = '1.0.1'
 
-try:
-    from OMMBV import igrf
-    from OMMBV import fortran_coords
-    from OMMBV import sources
-except ImportError:
-    # Warning about lack of import handled in trans
-    igrf, sources, fortran_coords = None, None, None
+# try:
+from OMMBV import igrf
+from OMMBV import fortran_coords
+from OMMBV import sources
+# except ImportError:
+#     # Warning about lack of import handled in trans
+#     igrf, sources, fortran_coords = None, None, None
 
 from OMMBV import satellite
 from OMMBV import trace
