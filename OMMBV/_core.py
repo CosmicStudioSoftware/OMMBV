a73594c24c4556d74a72e9181ae059e36496a8c5
Russell Stoneback <rstoneba@utdallas.edu>
2025-04-05 14:27:54 -0500

    BUG: Removed old commented and unneeded code

--- OMMBV/_core.py
+++ OMMBV/_core.py
@@ -292,13 +292,6 @@ def calculate_mag_drift_unit_vectors_ecef(latitude, longitude, altitude,
         ecef_x, ecef_y, ecef_z = trans.geodetic_to_ecef(latitude, longitude,
                                                         altitude)
 
-    # This shouldn't have been pushed to the repo
-    # idx, = np.where(np.isnan(ecef_x))
-    # if len(idx) > 0:
-    #     print("Encountered nan starting ecef locations ", ecef_x[idx],
-    #           ecef_y[idx], ecef_z[idx], latitude[idx], longitude[idx],
-    #           altitude[idx])
-
     # Begin method calculation.
 
     # Magnetic field at root location
@@ -357,13 +350,6 @@ def calculate_mag_drift_unit_vectors_ecef(latitude, longitude, altitude,
                                               return_geodetic=True,
                                               ecef_input=True,
                                               **step_kwargs)
-
-        # This shouldn't be here
-        # idx, = np.where(np.isnan(tzx))
-        # if len(idx) > 0:
-        #     print("Encountered nan tzx values ", idx, ecef_x[idx],
-        #           ecef_y[idx], ecef_z[idx], tzx[idx], tzy[idx], tzz[idx],
-        #           latitude[idx], longitude[idx], altitude[idx])
 
         if centered_diff:
             # Negative step
@@ -435,15 +421,6 @@ def calculate_mag_drift_unit_vectors_ecef(latitude, longitude, altitude,
                 & (loop_num + 1 >= min_loops):
             # Reached terminating conditions
             repeat_flag = False
-            # print('Using position ', location_info(ecef_xz, ecef_yz, ecef_zz,
-            #                                   datetimes,
-            #                                   return_geodetic=True,
-            #                                   ecef_input=True,
-            #                                   **step_kwargs)[3:], loop_num)
-            # idx, = np.where(np.isnan(ecef_xz))
-            # if len(idx) > 0:
-            #     print("Encountered nan apex locations ", ecef_xz[idx],
-            #           ecef_yz[idx], ecef_zz[idx])
         else:
             # Store info into calculation vectors to refine next loop
             tzx, tzy, tzz = tzx2, tzy2, tzz2
